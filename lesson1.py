714e99b5f4ab36fbb2c6f1ca951b6b8b8190bfb4
Camilo Figueroa <cigotete@gmail.com>
2024-06-12 18:13:14 -0500

    Example with Agent in loop running functions.

--- lesson1.py
+++ lesson1.py
@@ -114,3 +114,40 @@ next_prompt = "Observation: {}".format(eval("37 + 20"))
 print(next_prompt)
 abot(next_prompt)
 print(abot.messages)
+
+
+print("=" * 50)
+print("\nExample with Agent in loop running functions.\n")
+print("=" * 50)
+
+ # Python regular expression to selection action
+action_re = re.compile('^Action: (\w+): (.*)$')
+
+# Implementing a loop
+def query(question, max_turns=5):
+    i = 0
+    bot = Agent(prompt)
+    next_prompt = question
+    while i < max_turns:
+        i += 1
+        result = bot(next_prompt)
+        print(result)
+        actions = [
+            action_re.match(a) 
+            for a in result.split('\n') 
+            if action_re.match(a)
+        ]
+        if actions:
+            # There is an action to run
+            action, action_input = actions[0].groups()
+            if action not in known_actions:
+                raise Exception("Unknown action: {}: {}".format(action, action_input))
+            print(" -- running {} {}".format(action, action_input))
+            observation = known_actions[action](action_input)
+            print("Observation:", observation)
+            next_prompt = "Observation: {}".format(observation)
+        else:
+            return
+
+question = """I have 2 dogs, a border collie and a scottish terrier. What is their combined weight"""
+query(question)
